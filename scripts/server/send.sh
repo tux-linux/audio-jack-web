8f1a68a81e2aa1e8488e56a1c94d7c54c79b2573
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-06 22:49:34 -0500

    Use base64+xz compression for sending webpages

--- scripts/server/send.sh
+++ scripts/server/send.sh
@@ -12,7 +12,7 @@ RAND_TMP_SUFFIX=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
 BASEPATH="/tmp/audio-jack-web-${RAND_TMP_SUFFIX}"
 mkdir -p "${BASEPATH}"
 BASE_URL="${1}"
-SOURCE=$(wget "${BASE_URL}" -q -O -)
+SOURCE=$(wget "${BASE_URL}" -q -O - | xz | base64)
 
 echo "${SOURCE}" | tr -d '\n' | while read -r -n 450 SPLIT_SOURCE; do
     echo "POCSAG1200:1:\"${SPLIT_SOURCE}\"" | ./pagerenc | ffmpeg -f s16le -ar 22050 -ac 1 -i - -ar 44100 "${BASEPATH}/output.wav" && play "${BASEPATH}/output.wav" || error_exit
