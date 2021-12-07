1fc23c0ccd9c45d742b3eb4d71b40ccbd4765270
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-06 20:59:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# audio-jack-web
+
+Or a way to browse the Internet between two computers with only two 3.5mm audio cables and some Morse/POCSAG fanciness.
---
+++ scripts/client/request.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+error_exit() {
+    [ -z "${1}" ] && CAUSE="Unknown error"
+    [ -z "${2}" ] && EXITCODE=1
+    echo "Something went wrong. Reason: ${1}."
+    exit ${EXITCODE}
+}
+
+RAND_TMP_SUFFIX=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
+BASEPATH="/tmp/audio-jack-web-${RAND_TMP_SUFFIX}"
+mkdir -p "${BASEPATH}"
+[ -z "${1}" ] && error_exit "Please specify a request URL"
+BASE_URL="${1}"
+ENCODED_OUTPUT=$(echo "${BASE_URL}" | base32 | tr -d "\n")
+echo "request.sh: Sending string '${ENCODED_OUTPUT}'"
+echo "${ENCODED_OUTPUT}" | ./cwwav -w 35 --output "${BASEPATH}/request.wav" || error_exit "Failed converting base32 to wav file"
+play "${BASEPATH}/request.wav" || error_exit "Failed playing audio. Check if sox is installed"
+rm -rf "${BASEPATH}"
+
+exit 0
