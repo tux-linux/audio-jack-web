fc9c226282abe14f9080a6a27b0959caa45bd10a
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-13 19:56:09 -0500

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Warning: this script will only work with Debian-based systems.
+sudo apt install -y build-essential git libsndfile-dev bash qt5-qmake qtbase5-dev qtwebengine5-dev haskell-stack cmake libpulse-dev pavucontrol
+
+cd cwwav/
+make && cp -f cwwav ../scripts/client/cwwav
+cd ..
+
+cd pagerenc/
+stack setup
+stack build --local-bin-path . --copy-bins
+cd ..
+
+cd multimon-ng/
+cmake .
+make
+cd ..
+
+cd audio-jack-web-qt/
+qmake
+make
+cd ..
