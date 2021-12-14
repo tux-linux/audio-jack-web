040e8e87b7369b986d579fddd58ec29ce54640a7
Nicolas Mailloux <nicolecrivain@gmail.com>
2021-12-13 21:49:22 -0500

    Add LICENSE

--- audio-jack-web-qt/main.cpp
+++ audio-jack-web-qt/main.cpp
@@ -7,6 +7,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qDebug() << "audio-jack-web-qt | Copyright (C) 2021 Nicolas Mailloux";
+    qDebug() << "Licensed under the GNU General Public License 3.0";
+    qDebug() << "See the LICENSE file for details.";
+
     if(QCoreApplication::arguments().contains("load-progress")) {
         loadProgressDialog w;
         w.show();
